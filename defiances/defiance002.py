144f2eaaeab8bcde4da8c755e362a58c2ea0f600
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 12:27:56 -0300

    Create defiance002.py

---
+++ defiances/defiance002.py
@@ -0,0 +1,6 @@
+#Program that asks for year, month, day and when running displays the message: You were born in year month day. Correct?
+print('====== DEFIANCE 002 ======')
+y = input('Year: ')
+m = input('Month: ')
+d = input('Day: ')
+print('Your were born in {} {} {}. Correct?'.format(y, m, a))
