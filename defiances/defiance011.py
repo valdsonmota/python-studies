c88ae3efd3c7d44c105392bbc0d1f4016729d865
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 15:52:51 -0300

    Update defiance011.py

--- defiances/defiance011.py
+++ defiances/defiance011.py
@@ -1,5 +1,5 @@
 #Program that reads the width and height of a wall in meters, calculates the area and amount of paint you need, knowing that each liter of paint paints an area of 2m².
-print('=' * 10, 'DESAFIO 011', '=' * 10)
+print('=' * 10, 'DEFIANCE 011', '=' * 10)
 alt = float(input('Enter the height of the wall in meters: '))
 lar = float(input('Enter the width of the wall in meters: '))
 mq = alt * lar
