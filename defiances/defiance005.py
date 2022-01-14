9862ce856c3ca4b9681a77bd27bf1e4eba888a67
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 12:57:10 -0300

    Update defiance005.py

--- defiances/defiance005.py
+++ defiances/defiance005.py
@@ -1,4 +1,4 @@
-#program that reads an number integer and displays its successor and predecessor on the screen.
+#Program that reads an number integer and displays its successor and predecessor on the screen.
 print('====== DEFIANCE 005 ======')
 ni = int(input('Enter a number integer: '))
 np = ni - 1
