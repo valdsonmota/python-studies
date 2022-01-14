6e36eb04b31e0deaa4c07b95d5ae54887e37fcef
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 14:12:33 -0300

    Create defiance009.py

---
+++ defiances/defiance009.py
@@ -0,0 +1,25 @@
+#Program that reads any integer and displays its multiplication table on the screen.
+print('====== DEFIANCE 009 ======')
+tab = int(input('Enter a number to see your multiplication table: '))
+print('---------------')
+res = tab * 1
+print('{} x  1 = {}'.format(tab, res))
+res = tab * 2
+print('{} x  2 = {}'.format(tab, res))
+res = tab * 3
+print('{} x  3 = {}'.format(tab, res))
+res = tab * 4
+print('{} x  4 = {}'.format(tab, res))
+res = tab * 5
+print('{} x  5 = {}'.format(tab, res))
+res = tab * 6
+print('{} x  6 = {}'.format(tab, res))
+res = tab * 7
+print('{} x  7 = {}'.format(tab, res))
+res = tab * 8
+print('{} x  8 = {}'.format(tab, res))
+res = tab * 9
+print('{} x  9 = {}'.format(tab, res))
+res = tab * 10
+print('{} x 10 = {}'.format(tab, res))
+print('---------------')
