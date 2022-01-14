e1c55c2b58560cd4c3ee924dab19b3c6907bddd4
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 12:27:42 -0300

    Create defiance001.py

---
+++ defiances/defiance001.py
@@ -0,0 +1,4 @@
+#Program that asks for a name and displays the following sentence: Hello, <name>! Nice to meet you!
+print('====== DEFIANCE 001 ======')
+name = input('What is your name? ')
+print('Hello {}! Nice to meet you!'.format(name))
