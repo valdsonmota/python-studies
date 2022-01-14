492129e86114009e983adc8828737d037d18f3f9
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 12:25:08 -0300

    Delete defiance003_code.py

--- defiances/defiance003_code.py
+++
@@ -1,6 +0,0 @@
-#Program that reads two numbers and displays the sum between them.
-print('====== DEFIANCE 003 ======')
-num1 = int(input('Enter the first number: '))
-num2 = int(input('Enter the second number: '))
-sum = num1 + num2
-print('The sum of {} and {} is: '.format(num1, num2, sum))
