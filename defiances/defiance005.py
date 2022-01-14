519a6f5261c7eb39fc4a759938948f10b7a2e731
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-01-14 12:56:20 -0300

    Create defiance005.py

---
+++ defiances/defiance005.py
@@ -0,0 +1,8 @@
+#program that reads an number integer and displays its successor and predecessor on the screen.
+print('====== DEFIANCE 005 ======')
+ni = int(input('Enter a number integer: '))
+np = ni - 1
+ns = ni + 1
+print('Analyzing the number: {}'.format(ni))
+print('The number predecessor is: {}'.format(np))
+print('The number successor is: {}'.format(ns))
