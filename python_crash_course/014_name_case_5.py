3f55cf77223bd56188b15aae2ace10d2f4630d95
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-02-26 11:49:03 -0300

    Create 014_name_case_5.py

---
+++ python_crash_course/014_name_case_5.py
@@ -0,0 +1,6 @@
+#Remove whitespace from names
+name = ' Valdson Mota '
+print('1° -', name, '-')
+print('2° -', name.rstrip(), '-')
+print('3° -', name.lstrip(), '-')
+print('4° -', name.strip(), '-')
