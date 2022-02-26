e65fd3b832c766dc6ebba012c776eb5356b59f13
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-02-25 22:36:50 -0300

    Create 011_name_cases_2.py

---
+++ curso_intensivo_de_python/011_name_cases_2.py
@@ -0,0 +1,5 @@
+#Uppercase and lowercase letters in names.
+name = 'vaLdson moTa'
+print('Lowercase:', name.lower())
+print('Uppercase:', name.upper())
+print('First uppercase letters:', name.title())
