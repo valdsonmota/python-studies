b19bdef2a5ea8caa78795e35bdb480d9fd9e04ef
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-02-26 11:17:29 -0300

    Update 010_name_cases_1.py

--- python_crash_course/010_name_cases_1.py
+++ python_crash_course/010_name_cases_1.py
@@ -1,3 +1,3 @@
 #Personal message 2.3
 name = 'Valdson Mota'
-print('Hey', name, ', did you know? If you are not pay for the product, then you are the product.')
+print('Hey', name, ', if you are not pay for the product, then you are the product.')
