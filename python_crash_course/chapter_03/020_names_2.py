b9190b681490c96b86c804f6155d2d1dcede47ee
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-02-27 12:12:34 -0300

    Create 020_names_2.py

---
+++ python_crash_course/chapter_03/020_names_2.py
@@ -0,0 +1,6 @@
+#My friend list commented.
+friends = ['thiago', 'ricardo', 'rodolfo', 'jailson']
+print(friends[0].title() + ' - Known as THOLDER')
+print(friends[1].title() + ' - Known as FoM')
+print(friends[3].title() + ' - Known as Skol')
+print(friends[2].title() + ' - Lives in the state of Paraíba')
