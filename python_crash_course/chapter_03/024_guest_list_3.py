16a4c6e7ccff9c5f1ca85ae27f2ba5d54d8b1bb8
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-01 19:25:35 -0300

    Create 024_guest_list_3.py

---
+++ python_crash_course/chapter_03/024_guest_list_3.py
@@ -0,0 +1,21 @@
+#Double the number of guests.
+guests = ['ricardo', 'rodolfo', 'vinicius']
+print('Current dinner guest list')
+print('-' * 50)
+print(guests[0].title(), '- Confirmed')
+print(guests[1].title(), '- Confirmed')
+print(guests[2].title(), '- Confirmed')
+print('-' * 50)
+#Adding new guests to the list.
+guests.append('jailson')
+guests.append('miguel')
+guests.append('raphael')
+print('Updated dinner guest list.')
+print('-' * 50)
+print('Hello', guests[3].title() + ',', 'I would like to invite you to dinner next weekend at my beach house.')
+print('Hello', guests[4].title() + ',', 'I would like to invite you to dinner next weekend at my beach house.')
+print('Hello', guests[5].title() + ',', 'I would like to invite you to dinner next weekend at my beach house.')
+print('-' * 50)
+print('New dinner guest list')
+print('-' * 50)
+print(guests) #Full list
