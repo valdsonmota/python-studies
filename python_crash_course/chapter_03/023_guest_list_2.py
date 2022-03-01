e220dfb22dddb1f565a1460a5640fab1e23708b3
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-01 19:43:53 -0300

    Update 023_guest_list_2.py

--- python_crash_course/chapter_03/023_guest_list_2.py
+++ python_crash_course/chapter_03/023_guest_list_2.py
@@ -2,16 +2,19 @@
 guests = ['thiago', 'ricardo', 'rodolfo']
 print('Dinner guest list')
 print('-' * 50)
-print('Hello', guests[0].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('Hello', guests[1].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('Hello', guests[2].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
+print(guests[0].title(), '- Answered the invitation.')
+print(guests[1].title(), '- Invitation sent.')
+print(guests[2].title(), '- Invitation sent.')
+#Removing Thiago from the list
 dropout_guests = guests.pop(0)
 print('-' * 50)
 print(dropout_guests.title(), 'will not be able to attend the dinner.')
+#Adding new guest to the list.
 guests.append('vinicius')
 print('-' * 50)
+#New full dinner guest list
 print('New dinner guest list')
 print('-' * 50)
-print('Hello', guests[0].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('Hello', guests[1].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
+print(guests[0].title(), '- Invitation sent.')
+print(guests[1].title(), '- Invitation sent.')
 print('Hello', guests[2].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
