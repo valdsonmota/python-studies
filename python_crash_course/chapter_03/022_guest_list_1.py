a12a163913390308a8927dd5b1f8dfd669ba5915
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-01 18:01:57 -0300

    Create 022_guest_list_1.py

---
+++ python_crash_course/chapter_03/022_guest_list_1.py
@@ -0,0 +1,5 @@
+#Dinner guest list
+guests = ['thiago', 'ricardo', 'rodolfo']
+print('Hello', guests[0].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
+print('Hello', guests[1].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
+print('Hello', guests[2].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
