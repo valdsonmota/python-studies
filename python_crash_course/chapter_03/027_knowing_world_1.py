6eada90a82b465042cd92660a8ab4bb15ad75c4a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-02 10:19:02 -0300

    Create 027_knowing_world_1.py

---
+++ python_crash_course/chapter_03/027_knowing_world_1.py
@@ -0,0 +1,32 @@
+#Knowing the world
+cities = ['bariloche', 'machu picchu', 'auckland', 'lisboa', 'madrid', 'paris', 'bruxelas', 'londres', 'dublin', 'toronto']
+#List in original order
+print('List in original order')
+print(cities)
+print('-' * 115)
+#Temporary alphabetical order list.
+print('Alphabetical order without changing the original list')
+print(sorted(cities))
+print('-' * 115)
+#Temporary reverse alphabetical order list.
+print('Reverse alphabetical order without changing the original list')
+print(sorted(cities, reverse=True))
+print('-' * 115)
+#List in original order
+print('List in original order')
+print(cities)
+print('-' * 115)
+#Invertive original list
+print('Invertive original list')
+cities.reverse()
+print(cities)
+print('-' * 115)
+#Reversing the original list again.
+print('Reversing the original list again.')
+cities.reverse()
+print(cities)
+print('-' * 115)
+#List in alphabetical order permanently.
+print('List in alphabetical order permanently.')
+cities.sort()
+print(cities)
