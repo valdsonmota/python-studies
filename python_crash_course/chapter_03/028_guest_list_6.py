f414d33204655e124971d05ea67754228e897d51
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-02 10:29:08 -0300

    Create 028_guest_list_6.py

---
+++ python_crash_course/chapter_03/028_guest_list_6.py
@@ -0,0 +1,6 @@
+#Numbers of people invited to dinner.
+guests = ['ricardo', 'rodolfo', 'vinicius', 'jailson', 'miguel', 'raphael']
+print('DINNER GUEST LIST')
+print('-' * 40)
+print('Numbers of people invited to dinner:', len(guests))
+print('-' * 40)
