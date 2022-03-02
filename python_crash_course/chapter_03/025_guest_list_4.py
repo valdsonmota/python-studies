35f6e70a60f52c4f7fdfcbf0ab14cb20ecab687f
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-01 23:33:17 -0300

    Update 025_guest_list_4.py

--- python_crash_course/chapter_03/025_guest_list_4.py
+++ python_crash_course/chapter_03/025_guest_list_4.py
@@ -2,7 +2,7 @@
 guests = ['ricardo', 'rodolfo', 'vinicius', 'jailson', 'miguel', 'raphael']
 print('>' * 21, 'NOTICE', '<' * 21)
 print('')
-print('Dear guests', guests[0].title() + ',', guests[1].title() + ',', guests[2].title() + ',', guests[3].title() + ',', guests[4].title(), 'and', guests[5].title() + '!' + '\nDue to a problem with the custom furniture factory, I will have to reduce the list to just two guests. My apologies!')
+print('Dear guests', guests[0].title() + ',', guests[1].title() + ',', guests[2].title() + ',', guests[3].title() + ',', guests[4].title(), 'and', guests[5].title() + '!' + '\n\nDue to a problem with the custom furniture factory, I will have to reduce the list to just two guests. My apologies!')
 print('-' * 50)
 invitation_canceled = guests.pop(0)
 print('Sorry', invitation_canceled.title() + ',', 'your invitation has been cancelled.')
