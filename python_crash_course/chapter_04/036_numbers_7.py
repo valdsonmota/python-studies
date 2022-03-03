8fcb56595ab4feeaef719de83292e53ff6938324
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-03 12:38:28 -0300

    Update 036_numbers_7.py

--- python_crash_course/chapter_04/036_numbers_7.py
+++ python_crash_course/chapter_04/036_numbers_7.py
@@ -1,3 +1,5 @@
-#Creating a list of multiples of 3 in the range 3 to 30
-multiples = [three for three in range(3,31,3)]
+#Code with the list of multiples of three in the range 3 to 30.
+multiples = []
+for three in range(3, 31, 3):
+    multiples.append(three)
 print(multiples)
