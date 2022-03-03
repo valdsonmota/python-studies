57b2f7cb43d3e365e14c1050f56571991eedfd3c
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-03 07:59:38 -0300

    Create 034_numbers_5.py

---
+++ python_crash_course/chapter_04/034_numbers_5.py
@@ -0,0 +1,7 @@
+#Creating list from one to a million and displaying the lowest value, highest value and sum of all numbers in the list.
+numbers = list(range(1,1000001))
+print('-' * 55)
+print('The lowest value is:', min(numbers))
+print('\nThe highest value is:', max(numbers))
+print('\nThe sum of all the numbers in the list is:', sum(numbers))
+print('-' * 55)
