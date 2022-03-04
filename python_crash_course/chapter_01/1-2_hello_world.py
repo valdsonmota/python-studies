a5cd1be602a8a1fa186491997b1949af03158af9
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-04 17:34:24 -0300

    Create 1-2_hello_world.py

---
+++ python_crash_course/chapter_01/1-2_hello_world.py
@@ -0,0 +1,8 @@
+#Exercise 1-2 - First code to give good luck in the course.
+print('Hello World!')
+
+#Using variables
+message_1 = 'Hello Python World!'
+message_2 = 'Hello Python Crash Course World!'
+print(message_1)
+print(message_2)
