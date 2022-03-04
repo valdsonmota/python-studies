7197ebc62a12e01e85f9ac93496befb38ff08265
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-04 17:34:30 -0300

    Delete 001_hello_world.py

--- python_crash_course/chapter_02/001_hello_world.py
+++
@@ -1,8 +0,0 @@
-#First code to give good luck in the course.
-print('Hello World!')
-
-#Using variables
-message_1 = 'Hello Python World!'
-message_2 = 'Hello Python Crash Course World!'
-print(message_1)
-print(message_2)
