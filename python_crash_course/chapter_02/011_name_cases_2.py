7b14b6e0fc563b0163c0dba9a9db73fa024e3279
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-08 16:35:37 -0300

    Delete 011_name_cases_2.py

--- python_crash_course/chapter_02/011_name_cases_2.py
+++
@@ -1,5 +0,0 @@
-#Uppercase and lowercase letters in names.
-name = 'vaLdson moTa'
-print('Lowercase:', name.lower())
-print('Uppercase:', name.upper())
-print('First uppercase letters:', name.title())
