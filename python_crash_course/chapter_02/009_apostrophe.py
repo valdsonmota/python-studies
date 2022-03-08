20052f757f31395b30f1729ecb538dfe9493bd1a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-08 16:35:26 -0300

    Delete 009_apostrophe.py

--- python_crash_course/chapter_02/009_apostrophe.py
+++
@@ -1,3 +0,0 @@
-#The apostrophe appears between a set of double quotes, so the Python interpreter will have no problem reading the string correctly.
-message = "One of Python's strengths is its diverse community."
-print(message)
