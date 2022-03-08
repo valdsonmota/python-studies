8b0f1c7785753623d42b248fe496687b360124d6
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-08 16:37:44 -0300

    Create apostrophe.py

---
+++ python_crash_course/chapter_02/apostrophe.py
@@ -0,0 +1,3 @@
+#The apostrophe appears between a set of double quotes, so the Python interpreter will have no problem reading the string correctly.
+message = "One of Python's strengths is its diverse community."
+print(message)
