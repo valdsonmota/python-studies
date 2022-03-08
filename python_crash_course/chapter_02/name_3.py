624f6124b01afbc74be5df15e275fc92b7e82472
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-08 16:37:58 -0300

    Create name_3.py

---
+++ python_crash_course/chapter_02/name_3.py
@@ -0,0 +1,7 @@
+#Concatenating or combining Strings
+first_name = 'valdson'
+last_name = 'mota'
+full_name = first_name + ' ' + last_name
+print('Hello, ' + full_name.title() + '!')
+message = 'Welcome ' + last_name.title() + ' to my code.'
+print(message)
