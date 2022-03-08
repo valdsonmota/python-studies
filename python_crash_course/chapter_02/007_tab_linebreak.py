e82de0708b23cc97cb8ed223a953bd1bd097ea4f
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-08 16:35:14 -0300

    Delete 007_tab_linebreak.py

--- python_crash_course/chapter_02/007_tab_linebreak.py
+++
@@ -1,8 +0,0 @@
-#Tab and Line Break
-print('Valdson') #normal
-print('')
-print('\tValdson') #tab
-print('')
-print('Name:\nValdson\nMota') #line break
-print('')
-print('Name:\n\tValdson\n\tMota') #tab and line break
