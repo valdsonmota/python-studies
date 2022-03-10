f896a0566066198b50244136859ab11259443739
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-10 20:49:26 -0300

    Create 3-03_your_own_list.py

---
+++ python_crash_course/chapter_03/3-03_your_own_list.py
@@ -0,0 +1,8 @@
+#Exercise 3-3 - Your Own List - List of some DJI drone models in 2022/02/27
+dji = ['DJI Mavic 3', 'DJI Air 2S', 'DJI Mini 2', 'DJI Mini SE', 'DJI FPV', 'Phantom 4 Pro', 'Phantom 4 Pro V2.0', 'Inspire 2']
+print('Series Mavic:\n\t', dji[0])
+print('Series Air\n\t', dji[1])
+print('Series Mini\n\t', dji[2], '\n\t', dji[3])
+print('Series FPV\n\t', dji[4])
+print('Series Phantom\n\t', dji[5], '\n\t', dji[6])
+print('Series Inspire\n\t', dji[7])
