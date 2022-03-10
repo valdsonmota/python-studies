1d4e388c1eed512405b79cf733702907e32ba241
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-10 20:48:37 -0300

    Delete 022_guest_list_1.py

--- python_crash_course/chapter_03/022_guest_list_1.py
+++
@@ -1,9 +0,0 @@
-#Dinner guest list
-guests = ['thiago', 'ricardo', 'rodolfo']
-print('Dinner guest list')
-print('-' * 50)
-print('Hello', guests[0].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('Hello', guests[1].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('Hello', guests[2].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
-print('-' * 50)
-print(guests) #Full list
