ff73ab28b04a1eb1c5f26cd03a20ab8822dc06e6
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-10 20:49:36 -0300

    Create 3-05_changing_guest_list.py

---
+++ python_crash_course/chapter_03/3-05_changing_guest_list.py
@@ -0,0 +1,26 @@
+#Exercise 3-5 - Changing Guest List
+guests = ['thiago', 'ricardo', 'rodolfo']
+print('Dinner guest list')
+print('-' * 50)
+print(guests[0].title(), '- Answered the invitation.')
+print(guests[1].title(), '- Invitation sent.')
+print(guests[2].title(), '- Invitation sent.')
+#Removing Thiago from the list
+dropout_guests = guests.pop(0)
+print('-' * 50)
+print(dropout_guests.title(), 'will not be able to attend the dinner.')
+print('-' * 50)
+#Adding new guest to the list.
+print('New guest:')
+print('-' * 50)
+guests.append('vinicius')
+print('Hello', guests[2].title() + ",", 'I would like to invite you to dinner next weekend at my beach house.')
+print('-' * 50)
+#New full dinner guest list
+print('New dinner guest list')
+print('-' * 50)
+print(guests[0].title(), '- Invitation sent.')
+print(guests[1].title(), '- Invitation sent.')
+print(guests[2].title(), '- Invitation sent.')
+print('-' * 50)
+print(guests) #Full list
