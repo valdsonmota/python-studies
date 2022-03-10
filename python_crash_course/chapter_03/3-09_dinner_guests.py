387693fb4259b3efea4587552d31431a61b0e578
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-10 20:50:00 -0300

    Create 3-09_dinner_guests.py

---
+++ python_crash_course/chapter_03/3-09_dinner_guests.py
@@ -0,0 +1,6 @@
+#Exercise 3-9 - Dinner Guests 
+guests = ['ricardo', 'rodolfo', 'vinicius', 'jailson', 'miguel', 'raphael']
+print('DINNER GUEST LIST')
+print('-' * 40)
+print('Numbers of people invited to dinner:', len(guests))
+print('-' * 40)
