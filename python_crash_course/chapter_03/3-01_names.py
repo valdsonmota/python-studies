fb9b0242ceb6a74a4f09730db07f19eaaf291d2a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-10 20:49:18 -0300

    Create 3-01_names.py

---
+++ python_crash_course/chapter_03/3-01_names.py
@@ -0,0 +1,6 @@
+#Exercise 3-1 - Names
+friends = ['thiago', 'ricardo', 'rodolfo', 'jailson']
+print(friends[0].title())
+print(friends[1].title())
+print(friends[2].title())
+print(friends[3].title())
