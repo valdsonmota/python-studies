3bc4aa6b457560dfca24fe53a45cbabf4fda1ceb
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-11 15:27:59 -0300

    Create 4-05_summing_a_million.py

---
+++ python_crash_course/chapter_04/4-05_summing_a_million.py
@@ -0,0 +1,7 @@
+#Exercise 4-5 - Summing a Million
+numbers = list(range(1,1000001))
+print('-' * 55)
+print('The lowest value is:', min(numbers))
+print('\nThe highest value is:', max(numbers))
+print('\nThe sum of all the numbers in the list is:', sum(numbers))
+print('-' * 55)
