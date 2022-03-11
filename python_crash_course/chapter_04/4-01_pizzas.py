2396dcb829722deba2005be487ff72b34186338f
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-11 15:27:41 -0300

    Create 4-01_pizzas.py

---
+++ python_crash_course/chapter_04/4-01_pizzas.py
@@ -0,0 +1,7 @@
+#Exercise 4-1 - Pizzas
+pizzas = ['portuguese', 'pepperoni', 'mozzarella']
+for pizza in pizzas:
+    print(pizza.title(), 'PIZZA')
+print('I like pizza on weekends!')
+print('The best time to eat pizza is at dinner.')
+print("But I don't like to eat pizza every day.")
