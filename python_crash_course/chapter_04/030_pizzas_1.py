30f6a6edf6fe9d3bc9ee6de2be39726afddad059
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-11 15:26:34 -0300

    Delete 030_pizzas_1.py

--- python_crash_course/chapter_04/030_pizzas_1.py
+++
@@ -1,7 +0,0 @@
-#My favorite pizzas using for loop
-pizzas = ['portuguese', 'pepperoni', 'mozzarella']
-for pizza in pizzas:
-    print(pizza.title(), 'PIZZA')
-print('I like pizza on weekends!')
-print('The best time to eat pizza is at dinner.')
-print("But I don't like to eat pizza every day.")
