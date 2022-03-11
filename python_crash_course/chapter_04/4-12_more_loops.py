cd4cfe0ecaaf5bfb56f1b584e0f418c5d285e944
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-11 15:28:34 -0300

    Create 4-12_more_loops.py

---
+++ python_crash_course/chapter_04/4-12_more_loops.py
@@ -0,0 +1,9 @@
+#Exercise 4-12 - More Loops
+foods = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+friend_foods = ['pizza', 'falafel', 'carrot cake', 'ice cream']
+print("Eric Matthes' favorite foods:")
+for food in foods:
+    print('\t', food.title())
+print("\nEric Matthes' friend's favorite foods:")
+for friend in friend_foods:
+    print('\t', friend.title())
