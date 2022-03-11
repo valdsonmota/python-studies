b466b8d1cfd0385ce4488b94e25a33e56f412c49
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-11 15:27:22 -0300

    Delete 039_slices_1.py

--- python_crash_course/chapter_04/039_slices_1.py
+++
@@ -1,6 +0,0 @@
-#Code with a list from 1 to 20, uses slices to display the first three numbers in the list, the three numbers in the middle of the list, and the last three numbers in the list.
-slices = [slice for slice in range(1,21)]
-print('Full list\n', slices)
-print('\nThe first three numbers on the list are:\n', slices[:3])
-print('\nThe three numbers in the middle of the list are:\n', slices[8:11])
-print('\nThe last three numbers on the list are:\n', slices[-3:])
