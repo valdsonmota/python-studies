3410c41e1319d1b1c207802114317e2fb6e6062a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-20 11:36:01 -0300

    Update 5-1_conditional_tests_1.py

--- python_crash_course/chapter_05/5-1_conditional_tests_1.py
+++ python_crash_course/chapter_05/5-1_conditional_tests_1.py
@@ -1,2 +1,8 @@
-#Exercise 5-1 Conditional Tests
-
+#Exercise 5-1 Conditional Tests - Drones
+brand = 'dji'
+models = ['mavic 3', 'air 2s', 'mini 2', 'mini se', 'fpv', 'phantom 4 pro', 'phantom 4 pro v2.0', 'inspire 2']
+for model in models:
+    if model.lower() == 'fpv':
+        print(brand.upper(), model.upper())
+    else:
+        print(brand.upper(), model.title())
