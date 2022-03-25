6ba0481ae4251df35df539007bba599d02ec6334
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-25 15:09:40 -0300

    Create 5-5_alien_colors_1.py

---
+++ python_crash_course/chapter_05/5-5_alien_colors_1.py
@@ -0,0 +1,10 @@
+#Exercise 5-5 Alien Colors - 1
+alien_color = 'green'
+if alien_color == 'green':
+    print('You have earned 5 points.')
+elif alien_color == 'yellow':
+    print('You have earned 10 points.')
+elif alien_color == 'red':
+    print('You have earned 15 points.')
+else:
+    print('That color does not exist.')
