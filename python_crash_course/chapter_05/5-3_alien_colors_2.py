40e81b2c06161cc46cd9157c847c8d343182efa6
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-25 15:09:17 -0300

    Create 5-3_alien_colors_2.py

---
+++ python_crash_course/chapter_05/5-3_alien_colors_2.py
@@ -0,0 +1,8 @@
+#Exercise 5-3 Alien Colors - 2
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('You have earned 5 points.')
+if alien_color == 'yellow':
+    print('\n')
+if alien_color == 'red':
+    print('\n')
