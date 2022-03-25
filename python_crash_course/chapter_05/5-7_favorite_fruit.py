38e10c6332769ed13307cc470932d2a5900ecb7e
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-25 15:10:09 -0300

    Create 5-7_favorite_fruit.py

---
+++ python_crash_course/chapter_05/5-7_favorite_fruit.py
@@ -0,0 +1,31 @@
+#Exercise 5-7 Favorite Fruit
+favorite_fruits = ['appel', 'pineappel', 'grape']
+fruit = 'appel'
+if fruit in favorite_fruits:
+    print('You really like', fruit.title() + '!')
+else:
+    print(fruit.title(), 'is not a favorite fruit.')
+
+fruit = 'banana'
+if fruit in favorite_fruits:
+    print('You really like', fruit.title() + '!')
+else:
+    print(fruit.title(), 'is not a favorite fruit.')
+
+fruit = 'pineappel'
+if fruit in favorite_fruits:
+    print('You really like', fruit.title() + '!')
+else:
+    print(fruit.title(), 'is not a favorite fruit.')
+
+fruit = 'watermelon'
+if fruit in favorite_fruits:
+    print('You really like', fruit.title() + '!')
+else:
+    print(fruit.title(), 'is not a favorite fruit.')
+
+fruit = 'grape'
+if fruit in favorite_fruits:
+    print('You really like', fruit.title() + '!')
+else:
+    print(fruit.title(), 'is not a favorite fruit.')
