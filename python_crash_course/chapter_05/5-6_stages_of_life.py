ea3bed16a89ab5c2d52baa26378d672a5214041a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-25 15:10:03 -0300

    Create 5-6_stages_of_life.py

---
+++ python_crash_course/chapter_05/5-6_stages_of_life.py
@@ -0,0 +1,16 @@
+#Exercise 5-6 Stage of Life
+age =  40
+if age < 2:
+    print('That age is a baby.')
+elif (age >= 2) and (age < 4):
+    print('That age is a toddler.')
+elif (age >= 4) and (age < 13):
+    print('That age is a kid.')
+elif (age >= 13) and (age < 20):
+    print('That age is a teenager.')
+elif (age >= 20) and (age < 65):
+    print('That age is a adult.')
+elif (age >= 65) and (age <= 122):
+    print('That age is a elder.')
+else:
+    print('That age is a alien.')
