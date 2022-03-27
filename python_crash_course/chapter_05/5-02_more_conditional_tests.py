1ea15001e65cd592b6b27826929699180498b94c
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-27 11:58:58 -0300

    Update 5-02_more_conditional_tests.py

--- python_crash_course/chapter_05/5-02_more_conditional_tests.py
+++ python_crash_course/chapter_05/5-02_more_conditional_tests.py
@@ -1,1 +1,11 @@
 #Exercise 5-2 More Conditional Tests
+people = ['claudia', 'vinicius', 'miguel', 'valdson']
+for person in people:
+    if person == 'claudia':
+        print(person.title(), 'is an adult')
+    elif person == 'vinicius':
+        print(person.title(), 'is an new adult')
+    elif person == 'miguel':
+        print(person.title(), 'is a baby')
+    else:
+        print(person.title(), 'is an old adult.')
