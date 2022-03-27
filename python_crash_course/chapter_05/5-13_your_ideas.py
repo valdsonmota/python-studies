778022df57ed0022d784b7e2b6e8aa0802c11dbe
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-27 11:24:26 -0300

    Create 5-13_your_ideas.py

---
+++ python_crash_course/chapter_05/5-13_your_ideas.py
@@ -0,0 +1,32 @@
+#Exercise 5-13 Your Ideas - Valdson's Barbecue
+materials = ['barbecue grill', 'coal', 'lighter', 'tables', 'chairs']
+foods = ['meats', 'pork sausage', 'garlic bread', 'onion']
+drinks = ['soda', 'sparkling water', 'wine', 'beer']
+guests = ['claudia', 'vinicius', 'miguel']
+checklist = ['coal', 'lighter', 'meats', 'beer', 'miguel']
+print('-' * 10, 'MATERIAL', '-' * 10)
+for material in materials:
+    if (material in checklist):
+        print(material.title(), '- Buy')
+    else:
+        print(material.title(), ' - I already have')
+print('-' * 10, 'FOODS', '-' * 10)
+for food in foods:
+    if food in checklist:
+        print(food.title(), '- Buy')
+    else:
+        print(food.title(), '- I already bought')
+print('-' * 10, 'DRINKS', '-' * 10)
+for drink in drinks:
+    if drink in checklist:
+        print(drink.title(), '- Buy')
+    else:
+        print(drink.title(), '- I already bought')
+print('-' * 10, 'GUESTS', '-' * 10)
+for guest in guests:
+    if guest == 'claudia':
+        print(guest.title(), '- Confirmed')
+    elif guest == 'vinicius':
+        print(guest.title(), '- Waiting confirmation' )
+    elif guest in checklist:
+        print(guest.title(), '- Send invitation')
