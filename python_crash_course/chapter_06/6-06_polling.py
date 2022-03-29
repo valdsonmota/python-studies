8529f5034ffa33d27692e068e0ddcccecd6c5f80
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-29 17:09:12 -0300

    Create 6-06_polling.py

---
+++ python_crash_course/chapter_06/6-06_polling.py
@@ -0,0 +1,19 @@
+#Exercise 6-6 Polling
+people = [
+    'claudia',
+    'vinicius',
+    'michel',
+    'valdson',
+    'ozzy'
+    ]
+polling = {
+    'claudia': 'python',
+    'vinicius': 'qlik sense',
+    'valdson': 'python'
+}
+print('-' * 10, 'POLLING', '-' * 10)
+for person in people:
+    if person in polling:
+        print('\n' + person.title(), 'thanks for answering the poll.')
+    else:
+        print('\n' + person.title() + ': Please answer the poll.')
