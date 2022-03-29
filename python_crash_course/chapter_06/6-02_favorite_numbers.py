643b56d25ef4dd41e2d4ce682e1dbe477a4c9155
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-29 11:28:35 -0300

    Create 6-02_favorite_numbers.py

---
+++ python_crash_course/chapter_06/6-02_favorite_numbers.py
@@ -0,0 +1,13 @@
+#Exercise 6-2 Favorite Numbers
+favorite_numbers = {
+    'claudia': 30,
+    'vinicius': 81,
+    'miguel': 21,
+    'valdson': 27,
+    'ozzy': 19
+    }
+print('-' * 8, 'FAVORITE NUMBERS', '-' * 8)
+for name, number in favorite_numbers.items():
+    print(name.title() +
+        "'s favorite number is",
+        str(number) + '.')
