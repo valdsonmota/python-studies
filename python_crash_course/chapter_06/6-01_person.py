9a35481afba7678fe0dfcad55d0b0fb7bdcd1269
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-28 23:40:06 -0300

    Create 6-01_person.py

---
+++ python_crash_course/chapter_06/6-01_person.py
@@ -0,0 +1,9 @@
+#Exercise 6-1 Person
+person = {
+    'first_name': 'claudia',
+    'last_name': 'santos',
+    'age': '35',
+    'city': 'maceio',
+    }
+for k, v in person.items():
+    print(k.title() + ':', v.title())
