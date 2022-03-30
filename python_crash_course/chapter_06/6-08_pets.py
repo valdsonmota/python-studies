6e98ebb2ee39cd9fc71a7236e0dc601a10db51b0
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-29 22:31:47 -0300

    Create 6-08_pets.py

---
+++ python_crash_course/chapter_06/6-08_pets.py
@@ -0,0 +1,21 @@
+#Exercise 6-8 Pets
+ozzy = {
+    'name': 'ozzy',
+    'kind': 'cat',
+    'mom': 'claudia'
+    }
+little_black = {
+    'name': 'little black',
+    'kind': 'dog',
+    'mom': 'cristina'
+    }
+pets = [ozzy, little_black]
+for pet in pets:
+    name = pet['name']
+    kind = pet['kind']
+    mom = pet['mom']
+    print(
+        '\nName:', name.title(),
+        '\nKind:', kind.title(),
+        '\nMom:', mom.title()
+        )
