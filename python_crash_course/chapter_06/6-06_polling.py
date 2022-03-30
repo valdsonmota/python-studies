10e49cb9cc239acf3fd13027a1400d13a1004495
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-30 11:52:38 -0300

    Update 6-06_polling.py

--- python_crash_course/chapter_06/6-06_polling.py
+++ python_crash_course/chapter_06/6-06_polling.py
@@ -11,9 +11,19 @@ polling = {
     'vinicius': 'qlik sense',
     'valdson': 'python'
 }
-print('-' * 10, 'POLLING', '-' * 10)
+print(
+    '-' * 10,
+    'POLLING',
+    '-' * 10
+)
 for person in people:
     if person in polling:
-        print('\n' + person.title(), 'thanks for answering the poll.')
+        print(
+            '\n' + person.title(),
+            'thanks for answering the poll.'
+        )
     else:
-        print('\n' + person.title() + ': Please answer the poll.')
+        print(
+            '\n' + person.title() +
+            ': Please answer the poll.'
+        )
