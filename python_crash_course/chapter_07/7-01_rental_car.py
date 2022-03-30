a0faa0059e48a6e56dd2343a16c53e7df13b7e7a
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-03-30 15:58:26 -0300

    Create 7-01_rental_car.py

---
+++ python_crash_course/chapter_07/7-01_rental_car.py
@@ -0,0 +1,8 @@
+#Exercise 7-1 Rental Car
+rental_car = input(
+                'What kind of car would you like to rent? '
+            )
+print(
+    'Let me see if I can find you a',
+    rental_car.title() + '.'
+)
