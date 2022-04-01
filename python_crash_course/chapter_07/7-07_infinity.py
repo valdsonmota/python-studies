1abf5ba4337006f77b2a162b37f341b116414f59
Valdson Mota <96583503+valdsonmota@users.noreply.github.com>
2022-04-01 11:19:03 -0300

    Create 7-07_infinity.py

---
+++ python_crash_course/chapter_07/7-07_infinity.py
@@ -0,0 +1,9 @@
+#Exercise 7-7 Infinity
+active = True
+while active:
+        print(
+            '| To the center of the black hole.' +
+            '\n/ To the center of the black hole.' +
+            '\n- To the center of the black hole.' +
+            '\n\ To the center of the black hole.'
+        )
